4c3bb5eb7dc1613876fec1aadeb817d8584f6b19
NANEUNSIK <skadmstlrek@naver.com>
2024-07-19 15:24:08 +0900

    #<fix> <pull오류>

--- kiosk/src/App.js
+++ kiosk/src/App.js
@@ -1,12 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-// import Cart from './components/Cart';
+import Cart from './components/Cart'; // 장바구니 컴포넌트
+import Locker from './components/Locker'; // 사물함 컴포넌트
+import ChooseLocker from './components/ChooseLocker';
+import ItemRegistration from './components/ItemRegistration';
+import ServiceSelection from './components/ServiceSelection';
+import QuantityChange from './components/QuantityChange';
 
 function App() {
   return (
-    <div className="App">
-      <div>안녕</div>
-    </div>
+    <Router>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <Link className="navbar-brand" to="/">My App</Link>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Cart</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/locker">Locker</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/chooselocker">Choose Locker</Link>
+              </li>
+              <li className='nav-item'>
+                <Link className='nav-link' to="/serviceselection">ServiceSelection</Link>
+              </li>
+              <li className='nav-item'>
+                <Link className='nav-link' to="/itemregistration">ItemRegistration</Link>
+              </li>
+              <li className='nav-item'>
+                <Link className='nav-link' to="/quantitychange">QuantityChange</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Cart />} /> {/* 기본 경로로 Cart 컴포넌트를 렌더링합니다. */}
+          <Route path="/locker" element={<Locker />} /> {/* /locker 경로로 Locker 컴포넌트를 렌더링합니다. */}
+          <Route path="/chooselocker" element={<ChooseLocker />} /> {/* /chooselocker 경로로 ChooseLocker 컴포넌트를 렌더링합니다. */}
+          <Route path="/itemregistration" element={<ItemRegistration />} /> {/* /chooselocker 경로로 ChooseLocker 컴포넌트를 렌더링합니다. */}
+          <Route path="/serviceselection" element={<ServiceSelection />} /> {/* /chooselocker 경로로 ChooseLocker 컴포넌트를 렌더링합니다. */}
+          <Route path="/quantitychange" element={<QuantityChange />} /> {/* /chooselocker 경로로 ChooseLocker 컴포넌트를 렌더링합니다. */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ kiosk/src/components/ServiceSelection.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Button, Container, Row, Col } from 'react-bootstrap';
+import { Add, NewReleases } from '@mui/icons-material';
+import Typography from '@mui/material/Typography'; // @mui/material에서 Typography 가져오기
+import { useNavigate } from 'react-router-dom';
+import '../styles/ServiceSelection.css';
+
+const ServiceSelection = () => {
+  const navigate = useNavigate();
+
+  const register = () => {
+    navigate('/ItemRegistration');
+  };
+
+  const quan = () => {
+    navigate('/QuantityChange')
+  }
+  return (
+    <Container className="service-container">
+      <Typography variant="h6" component="h2" className="title" gutterBottom>
+        원하는 서비스를 선택해주세요
+      </Typography>
+      <Row className="justify-content-center">
+        <Col xs={5} className="d-flex justify-content-center mb-3">
+          <Button className="service-button" onClick={quan}>
+            <Add /> 수량 변경
+          </Button>
+        </Col>
+        <Col xs={5} className="d-flex justify-content-center mb-3">
+          <Button className="service-button" onClick={register}>
+            <NewReleases /> 물품 등록
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default ServiceSelection;
