4e754dd1ff69f853b10a69d2555b8e3b9b68e27a
윤하현 <yhh9944@naver.com>
2024-07-22 13:38:20 +0900

    #<feat> <Update>

--- web/src/pages/Cart.js
+++ web/src/pages/Cart.js
@@ -5,7 +5,10 @@ const Cart = () => {
   return (
     <div className='outer-box'>
       <p>장바구니</p>
-      
+      <div className="cart-buttons">
+        <button className="cart-button">예약하기</button>
+        <button className="cart-button cancel">취소</button>
+      </div>
     </div>
   );
 };
