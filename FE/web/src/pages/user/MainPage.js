5e14ab5ebf980764bfe4ab4b5b61852ab35b5465
윤하현 <yhh9944@naver.com>
2024-07-24 17:32:45 +0900

    #<feat> <request>

--- FE/web/src/pages/user/MainPage.js
+++ FE/web/src/pages/user/MainPage.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Nav, Button, Dropdown, Modal, Form } from 'react-bootstrap';
+import { Nav, Button, Dropdown } from 'react-bootstrap';
 import '../../style/MainPageApp.css'; 
 import { Link } from 'react-router-dom';
 import Profile from '../../images/profile.png';
@@ -9,14 +9,11 @@ import Cart from './Cart';
 import Modals from './Modals';
 import { CartProvider } from './CartContext';
 
-
-
 function MainPage() {
-
   const [show, setShow] = useState(false);
-
   const handleShow = () => setShow(true);
   const handleClose = () => setShow(false);
+
 
   return (
     <div>
@@ -27,10 +24,13 @@ function MainPage() {
         <Button variant="danger" onClick={handleShow}>
           요청
         </Button>
-        <Modals show={show} handleClose={handleClose} />
+        <Modals 
+          show={show} 
+          handleClose={handleClose} 
+        />
         <Dropdown>
           <Dropdown.Toggle id="dropdown-basic" className="custom-dropdown-toggle">
-          <img src={Profile} alt="프로필사진" style={{ width: '40px' }} />
+            <img src={Profile} alt="프로필사진" style={{ width: '40px' }} />
             홍길동 님
           </Dropdown.Toggle>
           <Dropdown.Menu>
@@ -41,48 +41,45 @@ function MainPage() {
       </nav>
 
       <div className="content-container">
-          <div className="side-bar">
-            <div>
-              미반납 물품
-              <ul>
-                <li>가위</li>
-                <li>풀</li>
-              </ul>
-            </div>
-            <hr></hr>
-            <div>
-              예약 물품
-              <ul>
-                <li>가위</li>
-                <li>풀</li>
-              </ul>
-            </div>
-            <hr></hr>
-            <div>
-              최근 대여 물품
-              <ul>
-                <li>가위 <button className='btn'>담기</button></li> 
-                <li>풀 <button className='btn'>담기</button></li>
-              </ul>
-            </div>
-            <hr></hr>
+        <div className="side-bar">
+          <div>
+            미반납 물품
+            <ul>
+              <li>가위</li>
+              <li>풀</li>
+            </ul>
           </div>
-          <div className="content">
-            <CartProvider>
+          <hr />
+          <div>
+            예약 물품
+            <ul>
+              <li>가위</li>
+              <li>풀</li>
+            </ul>
+          </div>
+          <hr />
+          <div>
+            최근 대여 물품
+            <ul>
+              <li>가위 <button className='btn'>담기</button></li> 
+              <li>풀 <button className='btn'>담기</button></li>
+            </ul>
+          </div>
+          <hr />
+        </div>
+        <div className="content">
+          <CartProvider>
             <div className="locker">
               <Locker />
             </div>
             <div className="cart">
               <Cart />
             </div>
-            </CartProvider>
-          </div>
+          </CartProvider>
+        </div>
       </div>
-    
     </div>
   );
 }
 
 export default MainPage;
-
-
