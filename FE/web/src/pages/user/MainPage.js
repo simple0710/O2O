9891d91046caad92dddb79342bcd24a57f81cff7
윤하현 <yhh9944@naver.com>
2024-07-24 13:38:56 +0900

    #<feat> <Update>

--- FE/web/src/pages/user/MainPage.js
+++ FE/web/src/pages/user/MainPage.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import Profile from '../../images/profile.png';
 import Locker from './Locker';
 import Cart from './Cart';
+import Modals from './Modals';
 import { CartProvider } from './CartContext';
 
 
@@ -14,8 +15,8 @@ function MainPage() {
 
   const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
 
   return (
     <div>
@@ -26,44 +27,7 @@ function MainPage() {
         <Button variant="danger" onClick={handleShow}>
           요청
         </Button>
-
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>물품 요청하기</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-
-          <Form.Group className="mb-3" controlId="formGridAddress1">
-            <Form.Label>물품 명</Form.Label>
-            <Form.Control placeholder="요청 물품 명을 적어주세요." />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formGridAddress2">
-            <Form.Label>신청 사유</Form.Label>
-            <Form.Control placeholder="물품 신청 사유를 적어주세요." />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formGridAddress2">
-            <Form.Label>물품 링크</Form.Label>
-            <Form.Control placeholder="물품 신청 사유를 적어주세요." />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formGridAddress2">
-            <Form.Label>물품 개수</Form.Label>
-            <Form.Control placeholder="물건 개수를 입력해주세요.(숫자만 입력해주세요)" />
-          </Form.Group>
-            
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              확인
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              취소
-            </Button>
-          </Modal.Footer>
-        </Modal>
-
+        <Modals show={show} handleClose={handleClose} />
         <Dropdown>
           <Dropdown.Toggle id="dropdown-basic" className="custom-dropdown-toggle">
           <img src={Profile} alt="프로필사진" style={{ width: '40px' }} />
