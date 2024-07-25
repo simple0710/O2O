18b1f059f5596625ff64b31705a7abb2c895347c
TaeYeonKim <tobysamsung@naver.com>
2024-07-25 15:40:32 +0900

    feat: 사용자 정보 불러오기, 변경 기능 생성

--- BE/o2o/src/main/java/com/one/o2o/controller/MemberController.java
+++ BE/o2o/src/main/java/com/one/o2o/controller/MemberController.java
@@ -16,12 +16,27 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/regist")
+    // 회원가입하는 코드
     public ResponseEntity<?> registmember(@RequestBody MemberDto memberDto){
         MemberEntity memberEntity = MemberEntity.toEntity(memberDto);
         // 여기 Entity에서 Dto를 통해서 만드는걸로!
         System.out.println(memberEntity);
 
-
+        // 회원가입이 성공하면 return true 실패하면 false 값을 준다!
         return new ResponseEntity<>(memberService.registmember(memberEntity), HttpStatus.OK) ;
+    }
+
+
+    @GetMapping("/profile/{user-id}")
+    public ResponseEntity<?> registmember(@PathVariable("user-id") int user_id){
+        // 서버에게 user-id (PK 값 1, 2, 3) 이런식의 값을 주면 사용자 프로필 정보를 get 해줌!
+        return new ResponseEntity<>(memberService.searchprofile(user_id) , HttpStatus.OK) ;
+    }
+
+    @PostMapping("/profile/{user-id}/edit")
+    public ResponseEntity<?> registmember(@PathVariable("user-id") int user_id, @RequestBody MemberEntity memberEntity) throws Throwable {
+        // 서버에게 MemberEntity를 주면 그 user-id (PK 값 1, 2, 3)에 따라 해당 사용자의 프로필 정보 변경!!
+        // 일단은 비밀번호랑 전화번호 정도만 바꿀수 있도록 했는데 더 추가하면되긴함!!
+        return new ResponseEntity<>(memberService.updateprofile(user_id, memberEntity) , HttpStatus.OK) ;
     }
 }
--- BE/o2o/src/main/java/com/one/o2o/service/MemberService.java
+++ BE/o2o/src/main/java/com/one/o2o/service/MemberService.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.function.Supplier;
+
 @Service
 @RequiredArgsConstructor
 public class MemberService {
@@ -25,6 +27,28 @@ public class MemberService {
         memberRepository.save(memberEntity);
         return true;
 
+    }
+
+    @Transactional
+    public MemberEntity searchprofile(int user_id){
+
+        return memberRepository.findById(user_id).get();
+    }
+
+    @Transactional
+    public MemberEntity updateprofile(int user_id, MemberEntity memberEntity) throws Throwable {
+        MemberEntity user_entity=  memberRepository.findById(user_id).orElseThrow(new Supplier<Throwable>() {
+            @Override
+            public Throwable get() {
+                return new IllegalArgumentException("수정에 실패하였습니다!");
+            }
+        });
+        user_entity.setUser_pw(memberEntity.getUser_pw());
+        user_entity.setUser_tel(memberEntity.getUser_tel());
+
+
+
+        return null;
     }
 
 
