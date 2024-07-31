e89ebe83070e8e05fba387a487b67ec1de712b62
이예림 <yr5698@gmail.com>
2024-07-31 16:44:35 +0900

    Revert "Merge branch 'feat/28-member' into 'master'"
    This reverts merge request !18

--- BE/o2o/src/main/java/com/one/o2o/controller/TestController.java
+++ BE/o2o/src/main/java/com/one/o2o/controller/TestController.java
@@ -5,5 +5,4 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/test")
 public class TestController {
-
 }
--- BE/o2o/src/main/java/com/one/o2o/repository/TestRepository.java
+++ BE/o2o/src/main/java/com/one/o2o/repository/TestRepository.java
@@ -1,5 +1,4 @@
 package com.one.o2o.repository;
 
-// jap 처리
 public class TestRepository {
 }
