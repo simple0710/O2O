5665e8bb5323110438f114ca86d83f018b491b94
김찬민 <thesimpleman@naver.com>
2024-08-01 17:49:05 +0900

    fix: 불필요한 데이터 삭제

--- BE/o2o/src/main/java/com/one/o2o/dto/reserve/ReserveRequestDto.java
+++ BE/o2o/src/main/java/com/one/o2o/dto/reserve/ReserveRequestDto.java
@@ -1,11 +1,9 @@
 package com.one.o2o.dto.reserve;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
