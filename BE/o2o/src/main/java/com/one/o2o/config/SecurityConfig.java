dedf2f468b5ffbcbc46bccb89301b05ece75fd38
yr5698 <yr5698@gmail.com>
2024-08-02 17:45:15 +0900

    fix: [BE] Security 정적 리소스 요청 처리

--- BE/o2o/src/main/java/com/one/o2o/config/SecurityConfig.java
+++ BE/o2o/src/main/java/com/one/o2o/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.one.o2o.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -51,5 +53,10 @@ public class SecurityConfig {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+
+    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring().mvcMatchers("/favicon.ico");
+        web.ignoring().requestMatchers(String.valueOf(PathRequest.toStaticResources().atCommonLocations()));
+    }
 
 }
