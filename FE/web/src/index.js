e8a8853f3b7592fd3cf3886686a53e32203484a3
yr5698 <yr5698@gmail.com>
2024-08-04 16:18:34 +0900

    [CICD] basename 수정

--- FE/web/src/index.js
+++ FE/web/src/index.js
@@ -11,7 +11,7 @@ import Footer from "./layout/Footer";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <BrowserRouter><App /></BrowserRouter>
+    <BrowserRouter basename='/frontend2'><App /></BrowserRouter>
   </>
 );
 
