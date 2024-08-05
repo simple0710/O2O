f137b5cff92357bf47be985c8830d551fa587e36
김찬민 <thesimpleman@naver.com>
2024-08-05 14:31:28 +0900

    fix: SignInDto/Json Naming 수정

--- BE/o2o/src/main/java/com/one/o2o/dto/SignInDto.java
+++ BE/o2o/src/main/java/com/one/o2o/dto/SignInDto.java
@@ -1,10 +1,13 @@
 package com.one.o2o.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class SignInDto {
     private String userLgid;
     private String userPw;
