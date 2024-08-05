95e751f9f80295a5cc586894924cfb6b19dfcfe6
윤하현 <yhh9944@naver.com>
2024-08-05 11:01:25 +0900

    fix: 로그인 수정

--- FE/web/src/utils/axiosInstance.js
+++ FE/web/src/utils/axiosInstance.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    // baseURL: 'http://localhost:8000', 
     timeout: 10000, 
 });
 
@@ -18,15 +17,34 @@ axiosInstance.interceptors.request.use(
     }
 );
 
-axiosInstance.interceptors.response.use(
-    response => response,
-    error => {
-        if (error.response && error.response.status === 401) {
-            localStorage.removeItem('accessToken');
-            window.location.href = '/'; 
-        }
-        return Promise.reject(error);
-    }
-);
+// axiosInstance.interceptors.response.use(
+//     response => response,
+//     async error => {
+//         const originalRequest = error.config;
+//         if (error.response && error.response.status === 401 && !originalRequest._retry) {
+//             originalRequest._retry = true;
+
+//             try {
+//                 const refreshToken = localStorage.getItem('refreshToken');
+//                 //리프레시 토큰을 사용해 새로운 액세스 토큰을 요청하는 URL
+//                 const response = await axios.post('', { refreshToken });
+//                 //새로운 토큰 발급
+//                 const newAccessToken = response.data.accessToken;
+//                 localStorage.setItem('accessToken', newAccessToken);
+
+//                 axios.defaults.headers.common['Authorization'] = `Bearer ${newAccessToken}`;
+//                 originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+
+//                 return axiosInstance(originalRequest);
+//             } catch (err) {
+//                 console.log(err);
+//                 localStorage.removeItem('accessToken');
+//                 localStorage.removeItem('refreshToken');
+//                 window.location.href = '/';
+//             }
+//         }
+//         return Promise.reject(error);
+//     }
+// );
 
 export default axiosInstance;
