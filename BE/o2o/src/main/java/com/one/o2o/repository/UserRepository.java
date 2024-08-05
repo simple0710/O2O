44465464ce5c0278e203e28a2238f7e2984337a3
yr5698 <yr5698@gmail.com>
2024-08-05 17:41:26 +0900

    feat: [BE] 사원증 이름 인식 API

--- BE/o2o/src/main/java/com/one/o2o/repository/UserRepository.java
+++ BE/o2o/src/main/java/com/one/o2o/repository/UserRepository.java
@@ -3,8 +3,10 @@ package com.one.o2o.repository;
 import com.one.o2o.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findAllByUserId(int userId);
+    List<User> findAllByUserNm(String userNm); //AndActiveIsTrue
 }
