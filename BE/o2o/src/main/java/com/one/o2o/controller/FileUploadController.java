b1d23e1ad426379c3f36187f7d7b005a5a27ae81
김찬민 <thesimpleman@naver.com>
2024-08-07 01:29:58 +0900

    feat: 이미지 로드 테스트 추가

--- BE/o2o/src/main/java/com/one/o2o/controller/FileUploadController.java
+++ BE/o2o/src/main/java/com/one/o2o/controller/FileUploadController.java
@@ -1,12 +1,13 @@
 package com.one.o2o.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -32,6 +33,21 @@ public class FileUploadController {
             return ResponseEntity.ok("파일 업로드 성공");
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("파일 업로드 실패");
+        }
+    }
+
+    @GetMapping("/images/{filename}")
+    public ResponseEntity<Resource> getImage(@PathVariable String filename) {
+        log.info("filename = {}", filename);
+        Path imagePath = Paths.get("/opt/homebrew/var/www/images/products/").resolve(filename);
+        Resource resource = new FileSystemResource(imagePath.toFile());
+
+        if (resource.exists()) {
+            return ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_JPEG) // 적절한 MIME 타입 설정
+                    .body(resource);
+        } else {
+            return ResponseEntity.notFound().build();
         }
     }
 
