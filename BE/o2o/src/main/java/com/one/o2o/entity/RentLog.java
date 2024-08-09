2761ff8dc24f218ad201b6f9c0fdc7e6e0272d8d
김찬민 <thesimpleman@naver.com>
2024-08-09 17:12:28 +0900

    fix: RentLog Entity 수정

--- BE/o2o/src/main/java/com/one/o2o/entity/RentLog.java
+++ BE/o2o/src/main/java/com/one/o2o/entity/RentLog.java
@@ -8,34 +8,43 @@ import lombok.ToString;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name="rent_log")
+@Table(name = "rent_log")
 @Getter
 @Setter
 @ToString
 public class RentLog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="log_id")
+    @Column(name = "log_id", nullable = false)
     private int id;
-    @ManyToOne(fetch=FetchType.LAZY)
-    @JoinColumn(name="rent_id", insertable = false, updatable = false)
-    private Rent rent;
-    @ManyToOne(fetch=FetchType.LAZY)
-    @JoinColumn(name="locker_id", insertable = false, updatable = false)
-    private Locker locker;
+
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="product_id", insertable = false, updatable = false)
+    @JoinColumn(name = "rent_id", insertable = false, updatable = false)
+    private Rent rent;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "locker_id", insertable = false, updatable = false)
+    private Locker locker;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", insertable = false, updatable = false)
     private Products products;
-    @Column(name="status_id", columnDefinition = "TINYINT(1)")
+
+    @Column(name = "status_id", columnDefinition = "TINYINT(1)", nullable = false)
     private int statusId;
-    @Column(name="log_cnt", columnDefinition = "SMALLINT")
+
+    @Column(name = "log_cnt", columnDefinition = "SMALLINT", nullable = false)
     private int logCnt;
+
+    @Column(updatable = false, nullable = false)
     private LocalDateTime logDt;
 
-    @Column(name="rent_id")
+    @Column(name = "rent_id", nullable = false)
     private int newRentId;
-    @Column(name="locker_id")
+
+    @Column(name = "locker_id", nullable = false)
     private int newLockerId;
-    @Column(name="product_id")
+
+    @Column(name = "product_id", nullable = false)
     private int newProductId;
 }
