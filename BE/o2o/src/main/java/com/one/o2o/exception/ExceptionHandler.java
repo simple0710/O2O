88bc840d65cad9ba15c8b12f36e260f00224fc7a
김찬민 <thesimpleman@naver.com>
2024-08-11 23:49:54 +0900

    fix: ExceptionHandler 이름 변경

--- BE/o2o/src/main/java/com/one/o2o/exception/ExceptionHandler.java
+++ BE/o2o/src/main/java/com/one/o2o/exception/ExceptionHandler.java
@@ -3,12 +3,11 @@ package com.one.o2o.exception;
 import com.one.o2o.exception.products.ProductException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class ProductExceptionHandler {
+public class ExceptionHandler {
 
-    @ExceptionHandler(ProductException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(ProductException.class)
     public ResponseEntity<ErrorResponse> handleProductException(ProductException ex) {
         ErrorResponse errorResponse = ErrorResponse.of(
                 ex.getErrorCode(),
