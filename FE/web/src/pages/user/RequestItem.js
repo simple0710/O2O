698562a3bbac2288e8a42175f91809bd75778d7f
최다희 <chdh8987@naver.com>
2024-08-12 10:00:03 +0900

    fix: 속성명 수정

--- FE/web/src/pages/user/RequestItem.js
+++ FE/web/src/pages/user/RequestItem.js
@@ -1,6 +1,147 @@
+// import React, {useState} from 'react';
+// import Sidebar from "./Sidebar";
+// import Nav from './Nav';
+// import { Button, Form } from 'react-bootstrap';
+// import '../../style/RequestItme.css'
+// import Swal from "sweetalert2";
+// import {postRequest} from '../../api/userpost'
+// import ButtonComponent from '../../components/ButtonComponent';
+
+// const RequestItem = () => {
+//     const userId = localStorage.getItem('userId');
+//     const [formData, setFormData] = useState({
+//         user_id: userId,
+//         itemname: '',
+//         requestreason : '',
+//         itemlink : '',
+//         itemcnt: ''
+//     })
+
+//     const handleChange = (e) => {
+//         const { name, value } = e.target;
+//         setFormData({
+//             ...formData,
+//             [name]: value
+//         });
+//     };
+
+//     const handleSubmit = async (e) => {
+//         e.preventDefault();
+//         console.log(formData);
+
+//         try{
+//             await postRequest(formData);
+//             console.log("Success")
+
+
+//             // 데이터 전송 후 input form 초기화
+//             setFormData({
+//                 user_id: userId,
+//                 itemname: '',
+//                 requestreason: '',
+//                 itemlink: '',
+//                 itemcnt: ''
+//             });
+
+//             Swal.fire({
+//                 title: "요청이 접수되었습니다.",
+//                 icon: "success",
+//                 confirmButtonColor: "#3085d6",
+//                 confirmButtonText: "확인",
+//               });
+//         } catch(e){
+//             console.error(e)
+//         }
+//     };
+
+  
+
+
+//     return (
+//         <div className="total">
+//             <Nav/>
+//             <div className="content-container">
+//                 <Sidebar/>
+//                 <div className='content'>
+//                     <h2>물품 요청</h2>
+//                     <div className='request-form'>
+//                         <form onSubmit={handleSubmit}>
+//                             <div>
+//                                 <label htmlFor="itemname" className='form-label'>물품명</label>
+//                                 <input
+//                                     type="itemname"
+//                                     className='form-control input-color'
+//                                     id='itemname'
+//                                     name='itemname'
+//                                     value={formData.itemname}
+//                                     onChange={handleChange}
+//                                     required
+//                                     placeholder='요청 물품명을 적어주세요.'
+//                                 />
+//                             </div>
+//                             <div>
+//                             <label htmlFor="requestreason" className='form-label'>신청 사유</label>
+//                                 <input
+//                                     type="requestreason"
+//                                     className='form-control input-color'
+//                                     id='requestreason'
+//                                     name='requestreason'
+//                                     value={formData.requestreason}
+//                                     onChange={handleChange}
+//                                     required
+//                                     placeholder='물품 신청 사유를 적어주세요.'
+//                                 />
+//                             </div>
+//                             <div>
+//                             <label htmlFor="itemlink" className='form-label'>물품 링크</label>
+//                                 <input
+//                                     type="itemlink"
+//                                     className='form-control input-color'
+//                                     id='itemlink'
+//                                     name='itemlink'
+//                                     value={formData.itemlink}
+//                                     onChange={handleChange}
+//                                     required
+//                                     placeholder='물품 링크를 입력해주세요.'
+//                                 />
+//                             </div>
+//                             <div>
+//                             <label htmlFor="itemcnt" className='form-label'>물품 개수</label>
+//                                 <input
+//                                     type="itemcnt"
+//                                     className='form-control input-color'
+//                                     id='itemcnt'
+//                                     name='itemcnt'
+//                                     value={formData.itemcnt}
+//                                     onChange={handleChange}
+//                                     required
+//                                     placeholder='물품 개수를 입력해주세요. (숫자만 입력해주세요)'
+//                                 />
+//                             </div>
+//                             <ButtonComponent 
+//                                 type="submit"
+//                                 style={{ marginTop: '20px' }}
+//                             >
+//                                 요청하기
+//                             </ButtonComponent>
+//                         </form>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+
+// export default RequestItem;
+
+
+
+
 import React, {useState} from 'react';
 import Sidebar from "./Sidebar";
 import Nav from './Nav';
+import { Button, Form } from 'react-bootstrap';
 import '../../style/RequestItme.css'
 import Swal from "sweetalert2";
 import {postRequest} from '../../api/userpost'
@@ -69,7 +210,7 @@ const RequestItem = () => {
                                 <label htmlFor="itemname" className='form-label'>물품명</label>
                                 <input
                                     type="itemname"
-                                    className='form-control'
+                                    className='form-control input-color'
                                     id='itemname'
                                     name='itemname'
                                     value={formData.itemname}
@@ -82,7 +223,7 @@ const RequestItem = () => {
                             <label htmlFor="requestreason" className='form-label'>신청 사유</label>
                                 <input
                                     type="requestreason"
-                                    className='form-control'
+                                    className='form-control input-color'
                                     id='requestreason'
                                     name='requestreason'
                                     value={formData.requestreason}
@@ -95,7 +236,7 @@ const RequestItem = () => {
                             <label htmlFor="itemlink" className='form-label'>물품 링크</label>
                                 <input
                                     type="itemlink"
-                                    className='form-control'
+                                    className='form-control input-color'
                                     id='itemlink'
                                     name='itemlink'
                                     value={formData.itemlink}
@@ -108,7 +249,7 @@ const RequestItem = () => {
                             <label htmlFor="itemcnt" className='form-label'>물품 개수</label>
                                 <input
                                     type="itemcnt"
-                                    className='form-control'
+                                    className='form-control input-color'
                                     id='itemcnt'
                                     name='itemcnt'
                                     value={formData.itemcnt}
