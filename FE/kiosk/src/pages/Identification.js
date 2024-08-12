31abad60197146726405d653d8fbe01fa44cde8e
yr5698 <yr5698@gmail.com>
2024-08-12 10:18:26 +0900

    fix: [FE] identification 충돌 수정

--- FE/kiosk/src/pages/Identification.js
+++ FE/kiosk/src/pages/Identification.js
@@ -441,6 +441,7 @@ function Identification() {
 
   }
   
+  
 
   const handleError = (msg) => {
     console.log("handleError ", msg);
@@ -479,4 +480,8 @@ function Identification() {
   );
 }
 
+<<<<<<< HEAD
 export default Identification;
+=======
+export default Identification;
+>>>>>>> f8e5947 (fix: [FE] identification 충돌 수정)
