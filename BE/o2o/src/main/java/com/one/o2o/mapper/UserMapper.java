02c13b9058407ab00cdcde9ff480965d4126e8f2
yr5698 <yr5698@gmail.com>
2024-08-12 15:36:26 +0900

    fix: [BE] user isActive, isAdmin 수정

--- BE/o2o/src/main/java/com/one/o2o/mapper/UserMapper.java
+++ BE/o2o/src/main/java/com/one/o2o/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package com.one.o2o.mapper;
 import com.one.o2o.dto.EmpCard.UserDto;
 import com.one.o2o.entity.Users;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
@@ -12,6 +13,8 @@ public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
     @Named("userToMemberDto")
+    @Mapping(source = "user.isActive", target = "active")
+    @Mapping(source = "user.isAdmin", target = "admin")
     UserDto userToUserDto(Users user);
 
 }
