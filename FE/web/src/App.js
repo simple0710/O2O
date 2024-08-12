991335e2d40d9a7e534035a1355148469eb15b82
김태연 <tobysamsung@naver.com>
2024-08-12 12:37:16 +0900

    Update FE/web/src/App.js

--- FE/web/src/App.js
+++ FE/web/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
 import MainPage from './pages/user/MainPage';
 import Login from './pages/Login';
 import Findpwd from './pages/Findpwd';
