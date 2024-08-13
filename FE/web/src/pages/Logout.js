141f9dcb550e9088232c5f34a8b064acaf9875ff
윤하현 <yhh9944@naver.com>
2024-08-14 00:43:39 +0900

    fix: 기능수정

--- FE/web/src/pages/Logout.js
+++ FE/web/src/pages/Logout.js
@@ -10,7 +10,7 @@ export const Logout = async (setCart, setLockerBodyId, navigate) => {
         localStorage.removeItem("lockerBodyId");
         setCart([]); // 상태 초기화
         setLockerBodyId(null); // 상태 초기화
-
+        // console.log('로그아웃 성공')
         navigate('/'); // 로그아웃 후 리다이렉트
     } catch (error) {
         console.error(error);
