13160244faa6911a81e04268f933fbaf082870fd
김태연 <tobysamsung@naver.com>
2024-08-13 16:44:19 +0900

    Update BE/o2o/src/main/java/com/one/o2o/controller/TestController.java

--- BE/o2o/src/main/java/com/one/o2o/controller/TestController.java
+++ BE/o2o/src/main/java/com/one/o2o/controller/TestController.java
@@ -10,7 +10,6 @@ public class TestController {
     @ResponseBody
     public void returnNoFavicon() {
     }
-출처: https://lloydkwon.tistory.com/entry/spring-mvc-favicon-에러 [새벽을 기다리는 파수꾼처럼:티스토리]
 
     @GetMapping("/test")
     public String testPage(){
